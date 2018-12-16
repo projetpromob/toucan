6a67599c235af011551e5fc43283652c4d5c2717
pierredamiendenis <denis.pierredamien@outlook.com>
2018-12-16 17:45:01 +0100

    Update Main_Activity_Accelerometer.java

--- app/src/main/java/promob/gospace/Accelerometer/Main_Activity_Accelerometer.java
+++ app/src/main/java/promob/gospace/Accelerometer/Main_Activity_Accelerometer.java
@@ -104,13 +104,10 @@ public class Main_Activity_Accelerometer extends Activity {
                                 mEngine.stop();
 
                                 Intent gameActivity = new Intent(Main_Activity_Accelerometer.this, GameOverActivity_Acce.class);
-                                Intent scorefinal = new Intent(Main_Activity_Accelerometer.this, promob.gospace.Multi.multi_activity.class);
-                                scorefinal.putExtra("scorefinal",final_time);
-                                startActivity(scorefinal);
-
+                                
                                 gameActivity.putExtra("scoreRecupA",final_time);
 
-                                //startActivity(gameActivity);
+                                startActivity(gameActivity);
 
 
                             }
