c58e933f77ea985151178a0d4fc024a1352b4eaa
pierredamiendenis <denis.pierredamien@outlook.com>
2018-12-16 21:29:17 +0100

    up

---
+++ app/src/main/java/promob/gospace/Multi/PageFin.java
@@ -0,0 +1,43 @@
+package promob.gospace.Multi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import promob.gospace.ChoixJoueurSolo;
+import promob.gospace.MainActivity;
+import promob.gospace.QCM.GameActivity;
+import promob.gospace.R;
+
+public class PageFin extends AppCompatActivity {
+
+    private Button Acceuil;
+    private TextView DisplayScore;
+    private String gagnant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        Acceuil = (Button) findViewById(R.id.acceuil_btn);
+        DisplayScore = (TextView) findViewById(R.id.displayScore);
+
+        Intent callingIntent = getIntent();
+        gagnant = callingIntent.getStringExtra("res");
+
+        DisplayScore.setText(gagnant);
+
+        Acceuil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Main = new Intent(PageFin.this, MainActivity.class);
+                startActivity(Main);
+            }
+        });
+
+    }
+}
